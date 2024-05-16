dea6692bf6221971bfd4501dcae94107c293eb4c
nurbek2107 <abduaxatov002@gmail.com>
2024-05-16 13:52:53 +0500

    main
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

--- main.js
+++ main.js
@@ -13,18 +13,32 @@
 const darkEL = document.getElementById('dark');
 const body = document.body;
 const header = document.getElementById('header');
-let isPlaying = true;
+const price = document.getElementById('price');
+let isPlaying = localStorage.getItem('isPlaying') === 'true';
 
-darkEL.addEventListener('click', () => {
+function toggleDarkMode() {
     if (isPlaying) {
         body.style.background = '#202C36';
         header.style.background = '#2B3844';
         header.style.color = "#fff";
-    } else {
-        body.style.background = 'white';
-        header.style.background = 'white';
-        header.style.color = "#000";
+        darkEL.innerHTML = '<i class="fa-solid fa-sun"></i>Light Mode';
+        price.style.background = '#2B3844';
+        price.style.color = '#fff';
 
+    } else {
+        body.style.background = '#f1f5f9';
+        header.style.background = 'white';
+        price.style.background = '#fff';
+        header.style.color = "#000";
+        price.style.color = '#000';
+        darkEL.innerHTML = '<i class="fa-regular fa-moon"></i>Dark Mode';
     }
+    localStorage.setItem('isPlaying', isPlaying);
+}
+
+darkEL.addEventListener('click', () => {
     isPlaying = !isPlaying;
+    toggleDarkMode();
 });
+
+toggleDarkMode();

adc2e641f5c6b2bf9b14f407c807864f2460f615
nurbek2107 <abduaxatov002@gmail.com>
2024-05-16 11:15:07 +0500

    main
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

---
+++ main.js
@@ -0,0 +1,11 @@
+// fetch("https://restcountries.com/v3.1/all").then((data) => {
+//     return data.json();
+// })
+//     .then((countires) => { 
+//         updation(countires)
+//     })
+// function updation(countires) {
+//         countires.forEach(element => {
+//             console.log(element.name)
+//         });
+//     }

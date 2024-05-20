80d13746470b7597575a6ed1cd021c7f857d9229
nurbek2107 <abduaxatov002@gmail.com>
2024-05-20 07:56:57 +0500

    main
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

--- app.js
+++ app.js
@@ -5,41 +5,30 @@ const price = document.getElementById('price');
 const cars = document.getElementById('cars');
 let isPlaying = localStorage.getItem('isPlaying') === 'true';
 
+function applyDarkModeStyles() {
+    darkEL.innerHTML = '<i class="fa-solid fa-sun"></i> Light Mode';
+    body.style.background = '#202C36';
+    header.style.background = '#2B3844';
+    header.style.color = "#fff";
+}
+function applyLightModeStyles() {
+    darkEL.innerHTML = '<i class="fa-regular fa-moon"></i> Dark Mode';
+    body.style.background = '#f1f5f9';
+    header.style.background = 'white';
+    header.style.color = "#000";
+}
 function toggleDarkMode() {
     isPlaying = !isPlaying;
+    localStorage.setItem('isPlaying', isPlaying);
     if (isPlaying) {
-        darkEL.innerHTML = '<i class="fa-solid fa-sun"></i> Light Mode';
-        body.style.background = '#202C36';
-        header.style.background = '#2B3844';
-        header.style.color = "#fff";
-        price.style.background = '#2B3844';
-        price.style.color = '#fff';
-        cars.style.background = '#2B3844';
-        cars.style.color = '#fff';
-        
+        applyDarkModeStyles();
     } else {
-        darkEL.innerHTML = '<i class="fa-regular fa-moon"></i> Dark Mode';
-        body.style.background = '#f1f5f9';
-        header.style.background = 'white';
-        header.style.color = "#000";
-        price.style.background = '#fff';
-        price.style.color = '#000';
-        cars.style.background = '#fff';
-        cars.style.color = '#000';
+        applyLightModeStyles();
     }
-
-    const listItems = document.querySelectorAll('.card');
-    listItems.forEach(item => {
-        item.style.background = isPlaying ? "#2B3844" : "#fff";
-        item.style.color = isPlaying ? "#fff" : "#000";
-    });
-
-    localStorage.setItem('isPlaying', isPlaying);  // Corrected the value stored in localStorage
 }
-
 darkEL.addEventListener('click', toggleDarkMode);
-
 if (isPlaying) {
-    toggleDarkMode();  // Apply dark mode styles if isPlaying is true on initial load
+    applyDarkModeStyles();  
+} else {
+    applyLightModeStyles(); 
 }
-
